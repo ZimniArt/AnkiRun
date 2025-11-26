8663eeb67247fd28afe2d2c881f62b58d72bda12
zimniart-admin <zimni.art@gmail.com>
2025-11-26 18:29:14 +0300

    vs studio project

---
+++ target/debug/deps/ankirun-766905fbfead9c38.d
@@ -0,0 +1,5 @@
+D:\Projects\AnkiRun\target\debug\deps\ankirun-766905fbfead9c38.d: src\main.rs
+
+D:\Projects\AnkiRun\target\debug\deps\libankirun-766905fbfead9c38.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/ankirun-99172648f7db1c93.d
@@ -0,0 +1,5 @@
+D:\Projects\AnkiRun\target\debug\deps\ankirun-99172648f7db1c93.d: src\main.rs
+
+D:\Projects\AnkiRun\target\debug\deps\libankirun-99172648f7db1c93.rmeta: src\main.rs
+
+src\main.rs:
